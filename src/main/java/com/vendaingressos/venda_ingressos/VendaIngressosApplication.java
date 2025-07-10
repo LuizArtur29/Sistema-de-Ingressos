90d39de85e7fdd02e493115d5619433b391df1cd
LuizArtur29 <luizarmartinsro99@gmail.com>
2025-07-10 11:21:08 -0300

    Commit
    feat: Adiciona estrutura inicial do projeto Spring Boot

---
+++ src/main/java/com/vendaingressos/venda_ingressos/VendaIngressosApplication.java
@@ -0,0 +1,13 @@
+package com.vendaingressos.venda_ingressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendaIngressosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendaIngressosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vendaingressos/venda_ingressos/VendaIngressosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendaingressos.venda_ingressos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendaIngressosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
