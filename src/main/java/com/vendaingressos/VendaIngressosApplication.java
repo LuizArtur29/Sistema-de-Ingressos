4ee1f3512f5fb7c7517ee4309cc6f222ebd5937b
Wolgrand <wolgrand.tecdev@gmail.com>
2025-11-04 14:52:45 -0300

    Update VendaIngressosApplication.java

--- src/main/java/com/vendaingressos/VendaIngressosApplication.java
+++ src/main/java/com/vendaingressos/VendaIngressosApplication.java
@@ -2,8 +2,10 @@ package com.vendaingressos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class VendaIngressosApplication {
 
 	public static void main(String[] args) {
