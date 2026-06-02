8034245d135a6ad1909833292a69617ebfa18b3d
LuizArtur29 <luiz.artur.coder@gmail.com>
2026-06-01 22:56:09 -0300

    feat: adicionando registro de usuario e corrigindo link de logins

--- frontend/services/auth.ts
+++ frontend/services/auth.ts
@@ -1,7 +1,12 @@
 import api from "./http";
-import { LoginRequest, LoginResponse } from "./types";
+import { LoginRequest, LoginResponse, RegisterRequest, RegisterResponse } from "./types";
 
 export async function login(payload: LoginRequest): Promise<LoginResponse> {
     const { data } = await api.post<LoginResponse>("/api/auth/login", payload);
+    return data;
+}
+
+export async function register(payload: RegisterRequest): Promise<RegisterResponse> {
+    const { data } = await api.post<RegisterResponse>("/api/usuarios", payload);
     return data;
 }
